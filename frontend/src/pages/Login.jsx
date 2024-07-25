50af837e98bf97d01c8709d636cceb42ef07d9f6
Dilanka <prasadliyanagamad@gmail.com>
2024-07-25 16:33:14 +0530

    login and register

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { post } from '../services/ApiEndpoint';
 
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(email, password);
+    try {
+      const request = await post('/api/auth/login', { email, password });
+      const response = request.data;
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="login">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="user-box">
+          <label htmlFor="Email">Email</label>
+          <input
+            type="email"
+            name=""
+            id="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="user-box">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name=""
+            id="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <button type="submit" className="btn">
+          Login
+        </button>
+        <p className="register-link">
+          Not registered?
+          <Link className="link" to={'/register'}>
+            Register here
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { post } from '../services/ApiEndpoint';
 
 const Register = () => {
-  return <div>Register</div>;
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const request = await post('/api/auth/register', {
+        name,
+        email,
+        password,
+      });
+      const response = request.data;
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div className="login">
+      <h2>Register</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="user-box">
+          <label htmlFor="Username">Username</label>
+          <input
+            type="text"
+            name=""
+            id="Username"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className="user-box">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name=""
+            id="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="user-box">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name=""
+            id="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <button type="submit" className="btn">
+          Register
+        </button>
+        <p className="register-link">
+          Already have an account?
+          <Link className="link" to={'/login'}>
+            Login here
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
